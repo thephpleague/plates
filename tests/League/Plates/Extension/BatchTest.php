5be0cf56de5a37610083ba8ab66302efa3db97ad
Jonathan Reinink <jonathan@reininks.com>
2013-11-14 21:52:14 -0500

    Add tests for Batch Extension.

---
+++ tests/League/Plates/Extension/BatchTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace League\Plates\Extension;
+
+class BatchTest extends \PHPUnit_Framework_TestCase
+{
+    private $extension;
+
+    public function setUp()
+    {
+        $this->extension = new Batch;
+        $this->extension->engine = new \League\Plates\Engine();
+        $this->extension->template = new \League\Plates\Template($this->extension->engine);
+    }
+
+    public function testCanCreateExtension()
+    {
+        $this->assertInstanceOf('League\Plates\Extension\Batch', $this->extension);
+    }
+
+    public function testGetFunctionsReturnsAnArray()
+    {
+        $this->assertInternalType('array', $this->extension->getFunctions());
+    }
+
+    public function testGetFunctionsReturnsAtLeastOneRecord()
+    {
+        $this->assertTrue(count($this->extension->getFunctions()) > 0);
+    }
+
+    public function testRunBatch()
+    {
+        $this->assertTrue($this->extension->runBatch('Sweet ', 'trim') === 'Sweet');
+        $this->assertTrue($this->extension->runBatch('Jonathan', 'strtoupper|strtolower') === 'jonathan');
+        $this->assertTrue($this->extension->runBatch('Jonathan', 'e|strtolower') === 'jonathan');
+    }
+}

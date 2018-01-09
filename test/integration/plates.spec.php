ce52888d4f9f28e95c89483f859cf36ac698434c
RJ Garcia <rj@bighead.net>
2018-01-08 18:12:50 -0800

    Fix Tests
    - Fixed broken tests
    - Added new tests
    - Removed var-dumper dep
    - Removed v4 example and moved it into an integration test
    
    Signed-off-by: RJ Garcia <rj@bighead.net>

---
+++ test/integration/plates.spec.php
@@ -0,0 +1,36 @@
+<?php
+
+use League\Plates\Engine;
+
+describe('Plates', function() {
+    it('can render templates with layouts, sections, relative paths, absolute paths, and folders', function() {
+        $plates = new Engine(['base_dir' => __DIR__ . '/fixtures/standard']);
+        $plates->addFolder('components', ['partials/components', '']);
+        $res = $plates->render('main', [
+            'name' => 'RJ & Emily',
+            'sidebar_style' => 'simple',
+        ]);
+        $html = <<<html
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile - RJ & Emily</title>
+    </head>
+    <body>
+        <div class="alert success">
+            <p>Success!</p>
+        </div>
+        <div class="alert error">
+            <p>Sorry...</p>
+        </div>
+        <h1>Hi RJ &amp; Emily</h1>
+<div id="sidebar" class="simple"></div>
+            <script type="text/javascript" id="main"></script>
+    <script type="text/javascript" id="sidebar"></script>
+            </body>
+</html>
+
+html;
+        expect($res)->equal($html);
+    });
+});
